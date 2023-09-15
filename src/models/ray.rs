6f0e77ef1b910f6bc23d7e71cf12b3f36feade4e
openhe-hub <625529334@qq.com>
2023-09-15 12:41:53 +0800

    2023-9-16-upd: upd hit sphere

--- src/models/ray.rs
+++ src/models/ray.rs
@@ -24,18 +24,28 @@ impl Ray {
         self.origin + self.dir.scale_mul(t)
     }
 
-    pub fn hit_sphere(&self, center: Point3, radius: f64) -> bool {
+    pub fn hit_sphere(&self, center: Point3, radius: f64) -> f64 {
         let center_vec = self.origin - center;
         let a = self.dir().dot(self.dir());
         let b = center_vec.dot(self.dir()) * 2.0;
         let c = center_vec.dot(center_vec) - radius * radius;
         let delta = b * b - 4.0 * a * c;
-        delta >= 0.0
+        if delta < 0.0 {
+            return -1.0;
+        } else {
+            return (-b - delta.sqrt()) / (2.0 * a);
+        }
     }
 
     pub fn ray_color(&self) -> Color {
-        if self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5) {
-            return Color::scale_to_rgb255(1.0, 0.0, 0.0);
+        let t = self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let normal_vec = (self.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+            return Color::scale_to_rgb255(
+                normal_vec.x() * 0.5 + 0.5,
+                normal_vec.y() * 0.5 + 0.5,
+                normal_vec.z() * 0.5 + 0.5,
+            );
         }
         let unit_dir: Vec3 = self.dir.unit();
         let a: f64 = 0.5 * (unit_dir.y() + 1.0);
